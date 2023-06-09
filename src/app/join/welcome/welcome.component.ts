1858831845ca4302ab4bc091f0deb134216fc35e
Georg <georg.strassberger@googlemail.com>
2023-06-09 22:29:10 +0200

    adding welcome template + greeting fn

--- src/app/join/welcome/welcome.component.ts
+++ src/app/join/welcome/welcome.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent {
+  daytime: string = '';
+  userName: string = 'Sophia Müller';
+  constructor() {
+    this.greeting();
+  }
 
+  greeting(): void {
+    const today = new Date();
+    const now = today.getHours() * 100 + today.getMinutes();
+    if (now <= 500) {
+      this.daytime = 'night';
+    } else if (now <= 1200) {
+      this.daytime = 'morning';
+    } else if (now <= 1700) {
+      this.daytime = 'afternoon';
+    } else {
+      this.daytime = 'evening';
+    }
+    console.log(`Current time: ${now}`);
+    console.log(`Daytime: ${this.daytime}`);
+  }
 }
