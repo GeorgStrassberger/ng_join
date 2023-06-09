f7ab3ab1544793da24241ac969154ec7291ddd7a
Georg <georg.strassberger@googlemail.com>
2023-06-09 21:59:13 +0200

    adding navbar template

--- src/app/join/header/header.component.ts
+++ src/app/join/header/header.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  openMenu: boolean = false;
 
+  onOpenMenu(): void {
+    this.openMenu = !this.openMenu;
+  }
 }
