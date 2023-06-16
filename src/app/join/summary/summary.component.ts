2574a1cf3ec4ad2cb75d1537afe69c897650d0bc
Georg <georg.strassberger@googlemail.com>
2023-06-16 18:15:46 +0200

    summary styling

--- src/app/join/summary/summary.component.ts
+++ src/app/join/summary/summary.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.scss']
+  styleUrls: ['./summary.component.scss', 'summary.media.scss'],
 })
-export class SummaryComponent {
-
-}
+export class SummaryComponent {}
