167638f66d6cb9e06240ad6caed5fae83cfb4a4d
Georg <georg.strassberger@googlemail.com>
2023-06-20 17:12:24 +0200

    adding routes to navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './join/welcome/welcome.component';
+import { ContactsComponent } from './join/contacts/contacts.component';
+import { SummaryComponent } from './join/summary/summary.component';
+import { BoardComponent } from './join/board/board.component';
+import { AddTaskComponent } from './join/add-task/add-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'summary', component: SummaryComponent },
+  { path: 'board', component: BoardComponent },
+  { path: 'addTask', component: AddTaskComponent },
+  { path: 'contacts', component: ContactsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
