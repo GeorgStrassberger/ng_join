de6435480b42ae6cc58562b6df337287f038122e
Georg <georg.strassberger@googlemail.com>
2023-06-21 15:53:05 +0200

    adding toa dd-contact-card popup notification

--- src/app/join/contacts/add-contact-card/add-contact-card.component.ts
+++ src/app/join/contacts/add-contact-card/add-contact-card.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-add-contact-card',
   templateUrl: './add-contact-card.component.html',
-  styleUrls: ['./add-contact-card.component.scss']
+  styleUrls: ['./add-contact-card.component.scss'],
 })
 export class AddContactCardComponent {
-
+  isCreated: boolean = false;
 }
